36eee5d5255ca92009079b8ff340806e5f3caeaf
dinaJamal <dj.developer.ri@gmail.com>
2022-10-11 02:17:43 +0300

    add seeders

--- index.ts
+++ index.ts
@@ -3,13 +3,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 import db from "./models";
 import { authors } from "./seeders/Authors";
+import { books } from "./seeders/Books";
 
-const createAuthors = () => {
-  authors.map((author) => {
-    db.Author.create(author);
+// const createAuthors = () => {
+//   authors.map((author) => {
+//     db.Author.create(author);
+//   });
+// };
+// createAuthors();
+
+const createBooks = () => {
+  books.map((book) => {
+    db.Book.create(book);
   });
 };
-createAuthors();
+createBooks();
+
 db.sequelize.sync().then(() => {
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
--- seeders/Authors.ts
+++ seeders/Authors.ts
@@ -1,10 +1,8 @@
 export const authors = [
     {
-        id: 1,
         name: 'Stephen Hawking'
     },
     {
-        id: 2,
         name: 'Dostoevsky'
     }
 ]
