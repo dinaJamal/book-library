0f698b3e3ae0bf45f03d6079a860598d61ee59d2
dinaJamal <dj.developer.ri@gmail.com>
2022-10-11 02:02:18 +0300

    first commit

--- index.ts
+++ index.ts
@@ -2,13 +2,14 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 import db from "./models";
-import { books } from "./seeders/Books";
+import { authors } from "./seeders/Authors";
 
-const createBooks = () => {
-  books.map((book) => {
-    db.Book.create(book);
+const createAuthors = () => {
+  authors.map((author) => {
+    db.Author.create(author);
   });
 };
+createAuthors();
 db.sequelize.sync().then(() => {
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
